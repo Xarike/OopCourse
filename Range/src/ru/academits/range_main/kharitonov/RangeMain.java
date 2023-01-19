7a726a95a01d9d341104bdffe3835fd3bbb54168
Xarike <khari.rabota@mail.ru>
2023-01-19 23:12:42 +0700

    Initial commit

--- Range/src/ru/academits/range_main/kharitonov/RangeMain.java
+++ Range/src/ru/academits/range_main/kharitonov/RangeMain.java
@@ -1,7 +1,43 @@
 package ru.academits.range_main.kharitonov;
 
+import ru.academits.range.kharitonov.Range;
+
 public class RangeMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Range range = new Range(10.5, 25.5);
+
+        double from = range.getFrom();
+        System.out.println("Начало диапазона " + from);
+
+        double to = range.getTo();
+        System.out.println("Конец диапазона " + to);
+
+        System.out.println("Длина диапазона равна " + range.getLength());
+
+        boolean isInNumbersRange = range.isInside(15.5);
+
+        if (isInNumbersRange) {
+            System.out.println("Число входит в диапазон!");
+        } else {
+            System.out.println("Число не входит в диапазон!");
+        }
+
+        range.setFrom(11.5);
+        from = range.getFrom();
+        System.out.println("Начало диапазона " + from);
+
+        range.setTo(30.5);
+        to = range.getTo();
+        System.out.println("Конец диапазона " + to);
+
+        System.out.println("Длина диапазона равна " + range.getLength());
+
+        isInNumbersRange = range.isInside(10.5);
+
+        if (isInNumbersRange) {
+            System.out.println("Число входит в диапазон!");
+        } else {
+            System.out.println("Число не входит в диапазон!");
+        }
     }
 }
