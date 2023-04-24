35d0df730e7709d0b8f2b3be842072d7d65f9fc1
Xarike <khari.rabota@mail.ru>
2023-04-25 00:52:17 +0700

    Initial commit

--- ArrayListHome/src/ru/academits/kharitonov/arrayListHome_main/Main.java
+++ ArrayListHome/src/ru/academits/kharitonov/arrayListHome_main/Main.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         try (Scanner scanner = new Scanner(new FileInputStream("TestList.txt"))) {
             ArrayList<Integer> list = new ArrayList<>();
+
             list.add(scanner.nextInt());
 
+
             System.out.println(list);
+        } catch (FileNotFoundException e ) {
+            System.out.println("Ошибка! Файл не найден!");
         }
     }
 }
